447f48543f1a9017861fd88f54a1cf3427a6b7d2
tori-3090ti <jsk4074@naver.com>
2025-09-16 14:22:06 +0900

    init

--- sans/utils.py
+++ sans/utils.py
@@ -83,6 +83,10 @@ def get_obj_from_str(string, reload=False):
     module, cls = string.rsplit(".", 1)
     if reload:
         module_imp = importlib.import_module(module)
+        print("TRRRRIGGGG")
+        print("TRRRRIGGGG")
+        print("TRRRRIGGGG")
+        print("TRRRRIGGGG")
         importlib.reload(module_imp)
     return getattr(importlib.import_module(module, package=None), cls)
 
