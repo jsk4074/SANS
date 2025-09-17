1be34fdc4399a03dbc760df7aa931f02652cba83
tori <jsk4074@naver.com>
2025-09-17 00:36:03 +0000

    commit

--- setup.py
+++ setup.py
@@ -35,9 +35,10 @@ VERSION = "0.1.3"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "torch>=1.13.0",
-    "torchaudio>=0.13.0",
-    "torchvision>=0.14.0",
+    # "torch",
+    # "torchaudio",
+    # "torchvision",
+    "matplotlib",
     "tqdm",
     "gradio",
     "pyyaml",
